d5105e655a8e54bc7ee2d09bf7c9e1fab3984032
Akemin-PC\Akemin <sirokumaorange1129@gamil.com>
2021-06-25 16:39:55 +0900

    修正

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,15 +1,18 @@
 import firebase from 'firebase'
+import firebase from "firebase/app"
+import "firebase/auth"
+import "firebase/firestore"
 
 if (!firebase.apps.length) {
   firebase.initializeApp(
     {
-        apiKey: "sampleDnGtkVa-frvtI3r7SOaFc7PKxAoANKPxA",
-        authDomain: "sample-nuxt-firebase-introduction.firebaseapp.com",
-        projectId: "sample-nuxt-firebase-introduction",
-        storageBucket: "sample-nuxt-firebase-introduction.appspot.com",
-        messagingSenderId: "sample-779950847276",
-        appId: "sample-1:750847276:web:dbf3b5a8bc3c4d641a6889",
-        measurementId: "sample-EKJ1X2XH3"
+      apiKey: "AIzaSyBo7JyGtJKm8JoDhy4ja48MoKABxcCSRX4",
+      authDomain: "fir-pj-47fbf.firebaseapp.com",
+      projectId: "fir-pj-47fbf",
+      storageBucket: "fir-pj-47fbf.appspot.com",
+      messagingSenderId: "1041825400337",
+      appId: "1:1041825400337:web:fb102aa3cdcaf0b24b13b0",
+      measurementId: "G-FKFCTMHX9Z"
     }
   )
 }
